9fd9664ece5c101d013d180ba558abc0a59cacfb
siskaandrianit <siskaandriani1906@gmail.com>
2019-04-11 07:44:38 +0700

    Updates

---
+++ precache-manifest.c138069fc4f0ef37662496056e7087a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/utsweb3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.0bfe0192.js"
+  },
+  {
+    "revision": "aef7a737c60eb8d2b9e4",
+    "url": "/utsweb3/static/js/main.4fb8ff5b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/utsweb3/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "aef7a737c60eb8d2b9e4",
+    "url": "/utsweb3/static/css/main.6bcda59d.chunk.css"
+  },
+  {
+    "revision": "a8a48b88003dd15f4217fc266b7e249f",
+    "url": "/utsweb3/index.html"
+  }
+];
